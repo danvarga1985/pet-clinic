d99b504947aaeed01f0690e4553c7d57022b3e94
danvarga1985 <danvarga1985@gmail.com>
2020-06-06 15:10:01 +0200

    Implement interfaces for services

---
+++ pet-clinic-data/src/main/java/daniel/varga/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package daniel.varga.petclinic.services;
+
+import daniel.varga.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet owner);
+
+    Set<Pet> findAll();
+}
