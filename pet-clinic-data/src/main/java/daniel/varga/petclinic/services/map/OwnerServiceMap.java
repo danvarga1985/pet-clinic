b11bac07e11e94f82436d6581a77ae8dc1da6bcf
danvarga1985 <danvarga1985@gmail.com>
2020-06-06 19:41:54 +0200

    Refactoring Service interface to common Base interface.
    Adding AbstactMapService interface, OwnerServiceMap, PerServiceMap & VetServiceMap classes.

---
+++ pet-clinic-data/src/main/java/daniel/varga/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,36 @@
+package daniel.varga.petclinic.services.map;
+
+import com.sun.xml.bind.v2.model.core.ID;
+import daniel.varga.petclinic.model.Owner;
+import daniel.varga.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        //.this would recurse indefinitely -> super is used
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
