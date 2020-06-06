7c9d9ecea36ab4b62d90fd1bdf77fe21105da131
danvarga1985 <danvarga1985@gmail.com>
2020-06-06 19:00:58 +0200

    Refactoring Service interface to common Base interface.

--- pet-clinic-data/src/main/java/daniel/varga/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/daniel/varga/petclinic/services/VetService.java
@@ -4,11 +4,5 @@ import daniel.varga.petclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
