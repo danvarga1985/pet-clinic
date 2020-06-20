e2ae9d283549cbebf1306cd89d0016d639a13637
danvarga1985 <danvarga1985@gmail.com>
2020-06-20 19:03:58 +0200

    Updating models with JPA-Entity mappings

--- pet-clinic-data/src/main/java/daniel/varga/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/daniel/varga/petclinic/model/Pet.java
@@ -1,15 +1,24 @@
 package daniel.varga.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
 
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
 
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     public String getName() {
