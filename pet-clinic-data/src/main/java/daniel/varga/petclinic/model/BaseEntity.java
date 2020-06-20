950b0f053263c68a0e921a0fe93ff0806d80efd6
danvarga1985 <danvarga1985@gmail.com>
2020-06-20 18:25:28 +0200

    Implementing BaseEntity

--- pet-clinic-data/src/main/java/daniel/varga/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/daniel/varga/petclinic/model/BaseEntity.java
@@ -1,10 +1,17 @@
 package daniel.varga.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
     //Hibernate prefers using boxed types (Long) as opposed to primitives (long), because boxed types can be null.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
