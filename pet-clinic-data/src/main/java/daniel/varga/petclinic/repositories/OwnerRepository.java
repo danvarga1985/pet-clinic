efcfeec2e24444356345bfaa65f891c73f27b059
danvarga1985 <danvarga1985@gmail.com>
2020-06-26 14:02:58 +0200

    Create Spring Data Jpa for Owner

--- pet-clinic-data/src/main/java/daniel/varga/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/daniel/varga/petclinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import daniel.varga.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    public Owner findByLastName(String lastName);
 }
