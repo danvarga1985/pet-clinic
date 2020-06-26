b7f663caa5f8f07799ba1cb7e85bdd3e79ec3f74
danvarga1985 <danvarga1985@gmail.com>
2020-06-26 16:03:57 +0200

    Create Spring Data Jpa for Owner

---
+++ pet-clinic-data/src/main/java/daniel/varga/petclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,50 @@
+package daniel.varga.petclinic.services.springdatajpa;
+
+import daniel.varga.petclinic.model.Vet;
+import daniel.varga.petclinic.repositories.VetRepository;
+import daniel.varga.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+
+        vetRepository.findAll().forEach(vets :: add);
+
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
