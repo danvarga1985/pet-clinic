bd40393c9134c0b3b90a3d222fa0514b914aa8fe
danvarga1985 <danvarga1985@gmail.com>
2020-06-28 09:56:29 +0200

    Adding Lombok to the project

--- pet-clinic-data/src/main/java/daniel/varga/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/daniel/varga/petclinic/model/Owner.java
@@ -1,9 +1,15 @@
 package daniel.varga.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
@@ -20,35 +26,14 @@ public class Owner extends Person {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
+    /* The @Builder annotation needs to be placed on a specific constructor in order to work, if there is more than one
+    constructor available. */
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String address, String city, String phone, Set<Pet> pets) {
+        super(id, firstName, lastName);
         this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
         this.city = city;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
         this.phone = phone;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
 }
